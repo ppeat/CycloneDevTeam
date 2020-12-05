cb60b2fca40648d7bd19e4292f9a7eb7f220325a
kuntzg <75282197+kuntzg@users.noreply.github.com>
2020-12-05 02:28:56 -0500

    w

---
+++ Version2(Alan)/Assets/wallmove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallmove : MonoBehaviour
+{
+
+    public float speed = 3f;
+    private Vector3 endPosition = new Vector3(0, -1.63f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != endPosition)
+        {
+            transform.position=Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);//moving wall in y direction
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        transform.localScale = transform.localScale + new Vector3(1 * Time.deltaTime, 1 * Time.deltaTime, 0); //wall object increases in size as it moves
+    }
+}
---
+++ Version2(Alan)/Assets/wallspawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallspawner : MonoBehaviour
+{
+
+    public GameObject wallcenter;
+    public GameObject wallleft;
+    public GameObject wallright;
+    public float maxpos = 2.2f;
+    float timer;
+    public float delay = 1f;//how often wall is spawned
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = delay;
+    }
+
+    void center()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) //when timer is less than zero a wall is spawned
+        {
+            Vector3 centerpos = new Vector3(0f, transform.position.y, transform.position.z);
+            Instantiate(wallcenter, centerpos, transform.rotation);
+            timer = delay;
+        }
+    }
+
+    void left()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) //when timer is less than zero a wall is spawned
+        {
+            Vector3 leftpos = new Vector3(-.05f, transform.position.y, transform.position.z);
+            Instantiate(wallleft, leftpos, transform.rotation);
+            timer = delay;
+        }
+    }
+
+    void right()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) //when timer is less than zero a wall is spawned
+        {
+            Vector3 rightpos = new Vector3(-.05f, transform.position.y, transform.position.z);
+            Instantiate(wallright, rightpos, transform.rotation);
+            timer = delay;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        center();
+        left();
+        right();
+    }
+}
